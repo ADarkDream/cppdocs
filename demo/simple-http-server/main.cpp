c97c9bf1f66253538c1d4df50f3602dd2dec4b0d
chenxuan <1607772321@qq.com>
2024-11-03 19:30:37 +0800

    feat:add web server

--- demo/simple-http-server/main.cpp
+++ demo/simple-http-server/main.cpp
@@ -7,23 +7,27 @@ using namespace std;
 
 int main() {
   HttpServer server;
-  // auto rc = server.Init({"127.0.0.1", 8080});
-  auto ssl_context = make_shared<SSLContext>();
-  auto rc =
-      ssl_context->InitSvrFile("./ssl/cert_demo.pem", "./ssl/cert_key.pem");
+  auto rc = server.Init({"127.0.0.1", 8080});
+  // auto ssl_context = make_shared<SSLContext>();
+  // auto rc =
+  //     ssl_context->InitSvrFile("./ssl/cert_demo.pem", "./ssl/cert_key.pem");
+  // if (rc != 0) {
+  //   std::cout << "init ssl failed " << ssl_context->err_msg() << std::endl;
+  //   return rc;
+  // }
+  // rc = server.InitSSL({"127.0.0.1", 8080}, ssl_context);
   if (rc != 0) {
-    std::cout << "init ssl failed" << std::endl;
-    return rc;
-  }
-  rc = server.InitSSL({"127.0.0.1", 8080}, ssl_context);
-  if (rc != 0) {
-    std::cout << "init server failed" << std::endl;
+    std::cout << "init server failed " << server.err_msg() << std::endl;
     return rc;
   }
   server.set_logger(std::make_shared<StdLogger>());
 
   server.GET("/hello", [&](HttpContext &ctx) {
     ctx.resp().Text(HttpStatusCode::OK, "hello world");
+  });
+
+  server.GET("/world", [](HttpContext &ctx) {
+    ctx.resp().Redirect("https://chenxuanweb.top");
   });
 
   server.StaticDir("/", "./blog-home");
